c0ca05fc086dc26da4c0b73bc03fd70645269142
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2025-11-25 10:54:05 +0000

    Refactor Order class to use List<Product> instead of ArrayList<Product> for products and improve product handling in constructor and getter methods.

--- Order.java
+++ Order.java
@@ -5,12 +5,27 @@ import java.util.List;
 final class Order {
     
     private int orderNumber;
-    private ArrayList<Product> products;
+    private List<Product> products;
     private int timestamp;
 
     public Order(int orderNumber, ArrayList<Product> products, int timestamp) {
         this.orderNumber = orderNumber;
         this.timestamp = timestamp;
+
+        // validate data (check if null / empty)
+        if (products == null || products.isEmpty()) {
+            this.products = Collections.emptyList();
+        }
+        else {
+            // create a temporary variable
+            List<Product> tempProducts = new ArrayList<>(products.size());
+            // loop through products
+            for (Product p: products) {
+                tempProducts.add(new Product(p)); // avoid using the same instance so create a new one
+            }
+            // return list that is unmodifiable
+            this.products = Collections.unmodifiableList(tempProducts);
+        }
     }
 
     /* GETTERS */
@@ -24,14 +39,18 @@ final class Order {
 
     public List<Product> getProducts() {
 
+        // validate if null
         if (products.isEmpty()) {
             return Collections.emptyList();
         }
 
+        // create a temporary variable
         List<Product> productsCopy = new ArrayList<>(products.size());
+        // loop through products
         for (Product p: products) {
-            productsCopy.add(new Product(p)); // copy each product
+            productsCopy.add(new Product(p)); // avoid using the same instance, so create a new product
         }
+        // return list that is unmodifiable
         return Collections.unmodifiableList(productsCopy);
     }
 
