af67908035870b3bf87c65d2ef3cf7fba6841734
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2025-11-25 17:48:15 +0000

    feat: add search(), add(), and remove() methods

---
+++ Inventory.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Inventory {
+
+    private static Inventory instance = null;
+
+    private static int ordersFilled;
+    private ArrayList<Product> products;
+
+    // empty constructor
+    private Inventory() {}
+
+    public static synchronized Inventory getInstance() {
+
+        // check if an instance is null
+        if (instance == null) {
+            // create instance
+            instance = new Inventory();
+        }
+        return instance;
+    }
+
+    public ArrayList<Product> getInventory() {
+        // create copy variable
+        ArrayList<Product> copy = new ArrayList<>();
+        if (!(products == null)) {
+            // copy all values into the new variable
+            copy.addAll(products);
+        }
+        // sort the copy arraylist
+        Collections.sort(copy);
+        // return
+        return copy;
+    }
+
+    public void add(Product product) {
+        // check if product is a null object
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        // add product to the products arraylist
+        products.add(product);
+    }
+
+    public void remove(Product product) {
+        // check if product is a null object
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null.");
+        }
+
+        // loop through products
+        for (Product p: products) {
+            // product is found
+            if (p == product) {
+                // remove product from the arraylist
+                products.remove(p);
+            }
+        }
+    }
+
+    public Product search(Product product) {
+        // check if product is a null object
+        if (product == null) {
+            throw new IllegalStateException("Product cannot be null.");
+        }
+
+        // loop through products
+        for (Product p: products) {
+            // product is found
+            if (p == product) {
+                return p; // return product object
+            }
+        }
+        // default return statement
+        return null;
+    }
+
+    public Order generateOrder(int[] productCodes) {
+        return null;
+    }
+
+}
